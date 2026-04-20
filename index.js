59211926305806f54f6957ea45cf8a27c38da45a
Vyannnnnn <fian88518@gmail.com>
2026-04-20 20:21:55 +0700

    Fix Vercel path resolution - use absolute paths

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.static("node_modules"));
 app.use(express.static("src/assets"));
 
-
-
 app.engine(
   "hbs",
   engine({
     extname: ".hbs",
     defaultLayout: false,
-    partialsDir: "./src/views/partials",
+    partialsDir: path.join(__dirname, "src/views/partials"),
   }),
 );
 app.set("view engine", "hbs");
-app.set("views", "./src/views");
+app.set("views", path.join(__dirname, "src/views"));
 
 // route
 app.get("/", (req, res) => {
